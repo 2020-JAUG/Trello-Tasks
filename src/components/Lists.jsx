053d8a4dab195a0437ae2f90b49881ece1ab9a8a
alejandrourbina <urbinagonzalezja@gmail.com>
2021-02-21 16:32:15 +0100

    orgaFolders

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Lists extends Component {
     render() {
         return (
-            <div>
+            <>
                 
-            </div>
+            </>
         )
     }
 }
