d8db95fa3851fe0b5e06a38198342ca19366778e
alejandrourbina <urbinagonzalezja@gmail.com>
2021-02-24 15:35:07 +0100

    Refactoring the code and delete files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
-import Cards from './components/Cards.jsx';
+import Card from './components/Card.jsx';
+import List from './components/List.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -8,12 +9,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-  <>
+    <>
     <Navbar/>
-    <Cards/>    
-      
-      
+    <Card/>  
+    <List titulo="Hola"/>
+ 
   </>
   );
 };
 export default App;
+
+
+//<Cards/>    
+//import Cards from './components/Cards.jsx';
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,35 +5,35 @@ import logo from '../assets/img/logo.png';
 const Navbar = () => ( 
 <> 
     <nav className="navbar navbar-expand-lg   navbar-light">
-        <button className="navbar-toggler collapsed navbar-toggle-rigth" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler navbar-toggle-rigth" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
-            <a className="navbar-logo" href="/#">
+            <a className="navbar-logo" href=".">
                 <img src={logo} alt="" width="33" height="30"></img>
             </a>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                    <a className="nav-link" href="/#">Home</a>
+                    <a className="nav-link bi bi-clipboard-data" alt="" href=".">{}</a>
                     </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="/#">Link</a>
+                        <a className="nav-link bi bi-house-door-fill" href=".">{}</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="/#">About</a>
+                        <a className="nav-link bi bi-share-fill" href="https://www.whatsapp.com/download" target="_blank" rel="noreferrer">{}</a>
                     </li>
                 </ul>
-                    <a className="navbar-brand hidden-md-down font-weight-bold" href="/#"><h3>Trello</h3></a>
+                    <a className="navbar-brand hidden-md-down font-weight-bold" href="https://blog.trello.com" target="_blank" rel="noreferrer"><h3>Trello</h3></a>
                
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                    <a className="nav-link" href="/#">Home</a>
+                    <a className="nav-link bi bi-github" href="https://github.com/2020-JAUG" target="_blank" rel="noreferrer">{}</a>
                     </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="/#">Link</a>
+                        <a className="nav-link bi bi-linkedin" href="https://www.linkedin.com/in/alejandro-urbina-gonzalez-686263169" target="_blank" rel="noreferrer">{}</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="/#">About</a>
+                        <a className="nav-link bi bi-twitter" href="https://twitter.com/AUrbina09807986" target="_blank" rel="noreferrer">{}</a>
                     </li>
                 </ul>
             </div>
