59720c721bc8a1d35040596ffbddec2bbe0508b2
alejandrourbina <urbinagonzalezja@gmail.com>
2021-02-24 10:03:05 +0100

    Updates files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import NavBar from './components/NavBar.jsx';
+import Navbar from './components/Navbar.jsx';
 import Cards from './components/Cards.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -8,7 +9,7 @@ import Cards from './components/Cards.jsx';
 function App() {
   return (
   <>
-    <NavBar/>
+    <Navbar/>
     <Cards/>    
       
       
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../assets/img/logo.png';
 
-const NavBar = () => ( 
-<>
-    <h1>Trello</h1>
-
-
+const Navbar = () => ( 
+<> 
+    <nav className="navbar navbar-expand-lg navbar-toggleable-md navbar-inverse navbar-light">
+        <button className="navbar-toggler navbar-toggle-rigth" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+            <a className="navbar-logo" href="/#">
+                <img src={logo} alt="" width="33" height="30"></img>
+            </a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/#">About</a>
+                    </li>
+                </ul>
+                 <a className="navbar-brand hidden-md-down" href="/#">Trello</a>
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/#">About</a>
+                        </li>
+                    </ul>
+            </div>
+    </nav>
 </>
 );
 
-export default NavBar;
+export default Navbar;
+
+/*</nav><a className="navbar-brand" href="#">Home</a>
+
+<nav className="navbar navbar-expand navbar-light flex-column flex-md-row bd-navbar">
+<ul className="nav justify-content-center">
+    <h2 nav className="justify-content-center">Trello</h2>
+</ul>
+
+</nav>
+*/
