c05d1c6f332519ece3d03b6497605f531c006554
alejandrourbina <urbinagonzalezja@gmail.com>
2021-03-03 09:40:11 +0100

    We add useEfect to save the tasks in  local storge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Navbar from './components/Navbar.jsx';
 import Card from './components/Card.jsx';
 import TaskRow from './components/TaskRow.jsx';
@@ -9,8 +9,26 @@ import AddList from './components/AddList.jsx';
 
 function App() {
 
+  //Tasks in local storage
+  let tasksIniciales = JSON.parse(localStorage.getItem('tasks'));
+  if( !tasksIniciales ) {
+    tasksIniciales = [];
+  }
+  
   //Array the all tasks
-  const [tasks, guardarTasks] = useState([]);
+  const [tasks, guardarTasks] = useState(tasksIniciales);
+
+  //UseEffect to update when there are changes on the component
+  useEffect( () => {
+    let tasksIniciales = JSON.parse(localStorage.getItem('tasks'));
+
+    if(tasksIniciales) {
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+    } else {
+      localStorage.setItem('tasks', JSON.stringify([]));
+    }
+
+  }, [tasks]);
 
   //Function to take the current tasks and take the new ones
   const crearTask = task => { guardarTasks ([ ...tasks, task ])};
